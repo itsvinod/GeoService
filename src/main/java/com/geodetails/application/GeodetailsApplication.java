fbf1aa9ebddbdd4a21c92667556b6cbb6ccac241
itsvinod <vino_java@rediffmail.com>
2022-05-29 11:36:00 +0100

    Update GeodetailsApplication.java
    int a=1;//main 2nd update added

--- src/main/java/com/geodetails/application/GeodetailsApplication.java
+++ src/main/java/com/geodetails/application/GeodetailsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication()
 public class GeodetailsApplication {
 	public static void main(String[] args) {
-		int a=1;//main
+		int a=1;//main 2nd update
 		SpringApplication.run(GeodetailsApplication.class, args);
 	}
 

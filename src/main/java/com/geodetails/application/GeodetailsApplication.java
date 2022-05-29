1407715ed347bf8f3e65e22cda1109e1640d95d9
itsvinod <vino_java@rediffmail.com>
2022-05-29 12:11:35 +0100

    Update GeodetailsApplication.java
    main commit

--- src/main/java/com/geodetails/application/GeodetailsApplication.java
+++ src/main/java/com/geodetails/application/GeodetailsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication()
 public class GeodetailsApplication {
 	public static void main(String[] args) {
-		int a=1;//main 2nd update
+		int a=1;//main 
 		SpringApplication.run(GeodetailsApplication.class, args);
 	}
 

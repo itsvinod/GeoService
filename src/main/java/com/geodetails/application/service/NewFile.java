f238a80c6ecae75a8e8c610bbcae23d25a1c8068
vinod <vino_java@rediffmail.com>
2022-06-12 18:36:12 +0100

    from IDE

--- src/main/java/com/geodetails/application/service/NewFile.java
+++ src/main/java/com/geodetails/application/service/NewFile.java
@@ -1,10 +1,10 @@
 package com.geodetails.application.service;
 
 public class NewFile {
-    //new file from branchC1
-    //changed
-    in b1 updated
+    //updated b2 in IDE 2nd time
+
+    /*in b1 updated
 
     again in b1 update new file
-
+*/
 }
